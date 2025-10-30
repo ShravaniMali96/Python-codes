8f024e33e59d5194e91ba6e6f08133dff0f9b5d7
Shravani Mali <shravanimmali@gmail.com>
2025-10-30 15:51:12 +0530

    Add files via upload
    added line plot program

---
+++ Matplotlib/line-plot.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
+plt.plot(x, y)
+
+# plt.xlabel("X-axis")
+# plt.ylabel("Y-axis")
+
+# plt.xlim(0, 6)
+# plt.ylim(0, 12)
+
+plt.title("Simple Line Plot")
+plt.show()
